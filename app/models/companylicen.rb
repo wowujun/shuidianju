64a932dc550683a778022f33cdbf8d530ee4407a
jancnzhangmin <jancnzhangmin@hotmail.com>
2016-01-22 08:11:35 +0800

    2016-1-22am

--- app/models/companylicen.rb
+++ app/models/companylicen.rb
@@ -4,5 +4,10 @@ class Companylicen < ActiveRecord::Base
   mount_uploader :statut, StatutUploader
   mount_uploader :threesyscert, ThreesyscertUploader
   mount_uploader :taxcert, TaxcertUploader
-  mount_uploader :creditrct, CreditrctUploader
+  mount_uploader :creditrat, CreditratUploader
+  mount_uploader :finastatement, FinastatementUploader
+  mount_uploader :orgcode, OrgcodeUploader
+  mount_uploader :nolitproof, NolitproofUploader
+  mount_uploader :nobankcert, NobankcertUploader
+  mount_uploader :certofaward, CertofawardUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   resources :achievements
   resources :clas
   resources :homes
+
+  root 'homes#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
